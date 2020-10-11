21261b7496d1523b4604947d208c793065fb176d
TMFV <68297974+TMFV@users.noreply.github.com>
2020-10-11 12:32:08 +0300

    Header+Menu-mobile

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,15 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open1]"),
-    closeModalBtn: document.querySelector("[data-modal-close1]"),
-    modal1: document.querySelector("[data-modal1]"),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle("modal-open1")
-    refs.modal1.classList.toggle("is-hidden");
+    document.body.classList.toggle("modal-open")
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
